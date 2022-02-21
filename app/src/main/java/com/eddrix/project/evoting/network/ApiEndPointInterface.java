38e93f2fa8fee7809ae0b57ce23e26c31a4cc3d5
Gideon <gideon.reach@gmail.com>
2022-02-21 21:53:22 +0530

    Added Retrofit

---
+++ app/src/main/java/com/eddrix/project/evoting/network/ApiEndPointInterface.java
@@ -0,0 +1,58 @@
+package com.eddrix.project.evoting.network;
+
+
+import java.util.Map;
+
+import retrofit2.Call;
+import retrofit2.http.FieldMap;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+
+
+public interface ApiEndPointInterface {
+
+
+
+//    @FormUrlEncoded
+//    @POST("/BACKEND/SIGN_UP.php")
+//    Call<Response>signUp(@FieldMap Map<String, Object> queryMap);
+//
+//
+//    @GET("/BACKEND/GET_SIGNUP_SPINNERS_DATA.php")
+//    Call<com.eddrix.app.models.Reg.spinners.Response>getSignUpSpinnersData();
+//
+//
+//    @FormUrlEncoded
+//    @POST("/BACKEND/LOGIN.php")
+//    Call<com.eddrix.app.models.Reg.login.Response>login(@FieldMap Map<String, Object> queryMap);
+//
+//    @FormUrlEncoded
+//    @POST("/BACKEND/feedback.php")
+//    Call<com.eddrix.app.models.feedback.Response>sendFeedback(@FieldMap Map<String, Object> queryMap);
+//
+//    @FormUrlEncoded
+//    @POST("/BACKEND/GET_USER_DETAILS.php")
+//    Call<com.eddrix.app.models.profile.Response>getProfile(@FieldMap Map<String, Object> queryMap);
+//
+//    @FormUrlEncoded
+//    @POST("/BACKEND/UPDATE_DETAILS.php")
+//    Call<com.eddrix.app.models.update_fileds.Response>updateProfileFields(@FieldMap Map<String, Object> queryMap);
+//
+//
+//
+//    @GET("/BACKEND/feed_spinner.php")
+//    Call<com.eddrix.app.models.feedback_issues.Response>getFeedbackIssues();
+//
+//    @GET("/BACKEND/UPDATE_FEATURES.php")
+//    Call<com.eddrix.app.models.update.Response>getUpdateFeatures();
+//
+//
+//    @GET("/BACKEND/CHECK_UPDATE.php")
+//    Call<com.eddrix.app.models.update.status.Response>checkUpdateStatus();
+//
+//
+
+
+
+}
---
+++ app/src/main/java/com/eddrix/project/evoting/network/AppUtils.java
@@ -0,0 +1,6 @@
+package com.eddrix.project.evoting.network;
+
+public class AppUtils {
+
+    final static String BASE_URL = "http://evoting.vugido.com/";
+}
